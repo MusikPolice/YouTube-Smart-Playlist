1aa418889234e2630a9b8fc0195871c5a1af9af4
Jonathan <jonfritz@gmail.com>
2024-04-23 21:01:56 -0400

    Fixed pulling videos that match a query from a channel using its handle. Need to add matching videos to the managed playlist, and cache credentials to avoid oauth dance on every execution

--- youtube-smart-playlist.py
+++ youtube-smart-playlist.py
@@ -19,7 +19,7 @@ def get_authenticated_service(client_secrets_file):
             "https://www.googleapis.com/auth/youtube.force-ssl"
         ]
     )
-    credentials = flow.run_local_server()
+    credentials = flow.run_local_server()   # TODO: cache this credentials object to avoid having to redo the oauth flow every time
     return googleapiclient.discovery.build("youtube", "v3", credentials=credentials)
 
 
@@ -117,11 +117,11 @@ def get_videos_in_playlist(playlist_id, youtube):
     return results
 
 
-# returns the unique channel id for the specified channel name, or None if the specified name does not exist
-def get_channel_id(channel_name, youtube):
+# returns the unique channel id for the specified channel handle, or None if the specified handle does not exist
+def get_channel_id(channel_handle, youtube):
     response = youtube.channels().list(
         part="id",
-        forUsername=channel_name
+        forHandle=channel_handle
     ).execute()
 
     if 'items' in response:
@@ -135,14 +135,13 @@ def get_channel_id(channel_name, youtube):
 # this method does not handle pagination - only the first page of results will be returned
 def get_videos_from_channel(query, channel_id, youtube):
     response = youtube.search().list(
-        # q=query,
+        q=query,
         part="id,snippet",
         channelId=channel_id,
         maxResults=10,
         order="date",
         type="video",
     ).execute()
-    print(response) # TODO: this response is empty for a known good channel_id?
 
     return [
         Video (
@@ -160,27 +159,33 @@ def main(args):
 
     # if the managed playlist doesn't exist in the authenticated account, create it
     playlists = get_playlists(youtube)
-    if not any(playlist.playlist_name == args.playlist_name for playlist in playlists):
-        managed_playlist_id = create_private_playlist(args.playlist_name, "Managed by YouTube-Smart-Playlist", youtube)
-        playlists.append(Playlist(managed_playlist_id, args.playlist_name))
-        print(f"Created managed playlist {args.playlist_name} with id {managed_playlist_id}")
+    if not any(playlist.playlist_name.lower() == args.managed_playlist_name.lower() for playlist in playlists):
+        managed_playlist_id = create_private_playlist(args.managed_playlist_name, "Managed by YouTube-Smart-Playlist", youtube)
+        playlists.append(Playlist(managed_playlist_id, args.managed_playlist_name))
+        print(f"Created managed playlist {args.managed_playlist_name} with id {managed_playlist_id}")
 
     # the list of video ids that already exist in the managed playlist
-    managed_playlist_id = next((pl.playlist_id for pl in playlists if pl.playlist_name == args.playlist_name), None)
-    print(f"Fetching existing videos in the {args.playlist_name} playlist with id {managed_playlist_id}")
+    managed_playlist_id = next((pl.playlist_id for pl in playlists if pl.playlist_name == args.managed_playlist_name), None)
+    print(f"Fetching existing videos in the {args.managed_playlist_name} playlist with id {managed_playlist_id}")
     managed_video_ids = [video.video_id for video in get_videos_in_playlist(managed_playlist_id, youtube)]
 
-    # TODO: list all videos from our allowlist of subscriptions that are unwatched, match any filters that were specified, and are not in managed_video_ids
-    # TODO: handle None as a return type if the channel name is bad
-    # TODO: we get a channel id, but no videos for that channel...
-    channel_id = get_channel_id("GeminiTay", youtube)
-    print(f"The channel id for GeminiTay is {channel_id}")
-    print(get_videos_from_channel("hermitcraft", channel_id, youtube))
+    # the list of videos that match the query in the target channel
+    print(f"Fetching videos that match the query string {args.query} from channel with handle {args.channel_handle}")
+    channel_id = get_channel_id(args.channel_handle, youtube)
+    print(f"The channel id for {args.channel_handle} is {channel_id}")
+    matching_video_ids = [video.video_id for video in get_videos_from_channel(args.query, channel_id, youtube)]
+
+    # TODO: there is no way to check watch status of any video :/
+
+    video_ids_to_add_to_managed_playlist = [video_id for video_id in matching_video_ids if video_id not in managed_video_ids]
+    print(video_ids_to_add_to_managed_playlist)
+    # TODO: add the videos to the managed playlist
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--playlist_name', type=str, help='The name of the managed playlist. This playlist will be created if it does not already exist.')
-    # TODO: need to add args for the channels to pull from and the query string to match (i.e. geminitay and bdubs, "hermitcraft" respectively)
+    parser.add_argument('--managed_playlist_name', type=str, required=True, help='The name of the managed playlist. This playlist will be created if it does not already exist.')
+    parser.add_argument('--channel_handle', type=str, required=True, help='The handle of the channel to pull unwatched videos from')    # TODO: support multiple channels
+    parser.add_argument('--query', type=str, help='An optional query string that video title/description text has to match in order to be added to the managed playlist.')
     args = parser.parse_args()
     main(args)
